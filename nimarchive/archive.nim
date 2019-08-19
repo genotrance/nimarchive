5eee121c43dd269d19ef29c31c847432a48083b6
Ganesh Viswanathan <dev@genotrance.com>
2019-08-19 13:16:53 -0500

    Use configure

--- nimarchive/archive.nim
+++ nimarchive/archive.nim
@@ -6,140 +6,48 @@ const
   path = getTempDir() / "nimarchive"
   adoth = path/"libarchive/libarchive/archive.h"
 
+  lzma = path/"liblzma"
+  lzmasrc = lzma/"src/liblzma"
+  zlib = path/"zlib"
+  libarchive = path/"libarchive"
+
 static:
-  gitPull("https://github.com/libarchive/libarchive", path/"libarchive", "libarchive/*")
+  var conFlags = ""
 
-  gitPull("https://github.com/kobolabs/liblzma", path/"liblzma", "src/liblzma\nsrc/common")
+  # liblzma
+  gitPull("https://github.com/kobolabs/liblzma", lzma)
+  for i in ["xz", "xzdec", "lzmadec", "lzmainfo", "shared"]:
+    conFlags &= " --disable-$#" % i
+  configure(lzma, "Makefile", conFlags)
+  make(lzmasrc, ".libs/liblzma.a", "-j 2")
 
-  gitPull("https://github.com/madler/zlib", path/"zlib", "*.h\n*.c")
+  # zlib
+  gitPull("https://github.com/madler/zlib", zlib)
+  configure(zlib, "configure.log", "--static")
+  make(zlib, "libz.a")
 
-  var configH =
-    when defined(Windows): """
-#define id_t short
-#define uid_t short
-#define gid_t short
-#define HAVE_WCSCPY 1
-#define HAVE_WCSLEN 1
-#define HAVE_WINCRYPT_H 1
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_STDINT_H 1
-#define HAVE_STDBOOL_H 1
+  putEnv("CFLAGS", "-DHAVE_LIBLZMA=1 -DHAVE_LZMA_H=1 -DHAVE_LIBZ=1 -DHAVE_ZLIB_H=1 -I" &
+    (lzmasrc/"api").replace("\\", "/").replace("C:", "/c") & " -I" & zlib.replace("\\", "/").replace("C:", "/c"))
 
-#define HAVE_DECL_SIZE_MAX 1
-#define HAVE_DECL_SSIZE_MAX 1
-"""
-    elif defined(Linux): """
-#define _XOPEN_SOURCE 500
-#define HAVE_ERRNO_H 1
-#define HAVE_LIMITS_H 1
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_FCNTL 1
-#define HAVE_FCNTL_H 1
-#define HAVE_WCHAR_H 1
-#define HAVE_WCSCPY 1
-#define HAVE_WCSLEN 1
-#define HAVE_STDINT_H 1
-#define HAVE_STDBOOL_H 1
-#define HAVE_PWD_H 1
-#define HAVE_GETPWUID_R 1
-#define HAVE_GETGRGID_R 1
-#define HAVE_GETGRNAM_R 1
-#define HAVE_DIRENT_H 1
-#define HAVE_FCHDIR 1
-#define HAVE_GRP_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_LSTAT 1
-#define HAVE_SYS_UTSNAME_H 1
-#define HAVE_POLL 1
-#define HAVE_POLL_H 1
-#define HAVE_POSIX_SPAWNP 1
-#define HAVE_PIPE 1
-#define HAVE_FORK 1
-#define HAVE_VFORK 1
-#define HAVE_SPAWN_H 1
-#define HAVE_SYS_STAT_H 1
-#define HAVE_SYS_TIME_H 1
-#define S_ISVTX __S_ISVTX
-#define u_char __u_char
-"""
-    elif defined(OSX): """
-#define HAVE_ERRNO_H 1
-#define HAVE_LIMITS_H 1
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_FCNTL 1
-#define HAVE_FCNTL_H 1
-#define HAVE_WCHAR_H 1
-#define HAVE_WCSCPY 1
-#define HAVE_WCSLEN 1
-#define HAVE_STDINT_H 1
-#define HAVE_STDBOOL_H 1
-#define HAVE_PWD_H 1
-#define HAVE_GETPWUID_R 1
-#define HAVE_GETGRGID_R 1
-#define HAVE_GETGRNAM_R 1
-#define HAVE_DIRENT_H 1
-#define HAVE_FCHDIR 1
-#define HAVE_GRP_H 1
-#define HAVE_STDLIB_H 1
-#define HAVE_LSTAT 1
-#define HAVE_SYS_UTSNAME_H 1
-#define HAVE_POLL 1
-#define HAVE_POLL_H 1
-#define HAVE_POSIX_SPAWNP 1
-#define HAVE_PIPE 1
-#define HAVE_FORK 1
-#define HAVE_VFORK 1
-#define HAVE_SPAWN_H 1
-#define HAVE_ARC4RANDOM_BUF 1
-"""
+  # libarchive
+  gitPull("https://github.com/libarchive/libarchive", libarchive)
 
-  configH &= """
-#define HAVE_LZMA_H 1
-#define HAVE_LIBLZMA 1
-#define HAVE_ZLIB_H 1
+  conFlags = ""
+  for i in ["lzma", "zlib", "bz2lib", "nettle", "openssl", "libb2", "lz4", "zstd", "xml2", "expat"]:
+    conFlags &= " --without-$#" % i
+  for i in ["shared", "bsdtar", "bsdcat", "bsdcpio", "acl"]:
+    conFlags &= " --disable-$#" % i
+  configure(libarchive/"build", "../configure")
+  configure(libarchive, "Makefile", conFlags)
 
-#define HAVE_CHECK_CRC32 1
-#define HAVE_CHECK_CRC64 1
-#define HAVE_CHECK_SHA256 1
-#define HAVE_DECODER_ARM 1
-#define HAVE_DECODER_ARMTHUMB 1
-#define HAVE_DECODER_DELTA 1
-#define HAVE_DECODER_IA64 1
-#define HAVE_DECODER_LZMA1 1
-#define HAVE_DECODER_LZMA2 1
-#define HAVE_DECODER_POWERPC 1
-#define HAVE_DECODER_SPARC 1
-#define HAVE_DECODER_X86 1
-#define HAVE_ENCODER_ARM 1
-#define HAVE_ENCODER_ARMTHUMB 1
-#define HAVE_ENCODER_DELTA 1
-#define HAVE_ENCODER_IA64 1
-#define HAVE_ENCODER_LZMA1 1
-#define HAVE_ENCODER_LZMA2 1
-#define HAVE_ENCODER_POWERPC 1
-#define HAVE_ENCODER_SPARC 1
-#define HAVE_ENCODER_X86 1
-"""
+  make(libarchive, ".libs/libarchive.a", "libarchive.la -j 2")
 
-  writeFile(path/"libarchive/libarchive/config.h", configH)
   writeFile(adoth, readFile(adoth) & "\n#include \"archive_entry.h\"\n")
 
   cDebug()
   cSkipSymbol(@["archive_read_open_file", "archive_write_open_file"])
 
-cIncludeDir(path/"libarchive/libarchive")
-
-cIncludeDir(path/"liblzma/src/liblzma/api")
-cIncludeDir(path/"liblzma/src/common")
-cIncludeDir(path/"liblzma/src/liblzma/lz")
-cIncludeDir(path/"liblzma/src/liblzma/lzma")
-cIncludeDir(path/"liblzma/src/liblzma/common")
-cIncludeDir(path/"liblzma/src/liblzma/check")
-cIncludeDir(path/"liblzma/src/liblzma/simple")
-cIncludeDir(path/"liblzma/src/liblzma/delta")
-cIncludeDir(path/"liblzma/src/liblzma/rangecoder")
-
-cIncludeDir(path/"zlib")
+  {.passL: libarchive/".libs/libarchive.a" & " " & lzmasrc/".libs/liblzma.a" & " " & zlib/"libz.a".}
 
 cPlugin:
   import macros, strutils
@@ -157,24 +65,8 @@ when defined(windows):
   type
     BY_HANDLE_FILE_INFORMATION = object
 
-{.passC: "-DHAVE_CONFIG_H -std=c99".}
+  {.passC: "-DHAVE_CONFIG_H -std=c99 -I" & libarchive.}
+  {.passL: "-lbcrypt".}
+  cCompile(path / "libarchive/libarchive/*_windows.c")
 
 cImport(adoth, recurse=true)
-
-cCompile(path / "libarchive/libarchive/archive*.c")
-
-when defined(windows):
-  cCompile(path / "libarchive/libarchive/filter_fork_windows.c")
-else:
-  cCompile(path / "libarchive/libarchive/filter_fork_posix.c")
-
-cCompile(path / "liblzma/src/liblzma/common")
-cCompile(path / "liblzma/src/liblzma/lzma", exclude="tablegen")
-cCompile(path / "liblzma/src/liblzma/lz")
-cCompile(path / "liblzma/src/liblzma/rangecoder", exclude="tablegen")
-cCompile(path / "liblzma/src/liblzma/check", exclude="small,tablegen")
-cCompile(path / "liblzma/src/liblzma/simple")
-cCompile(path / "liblzma/src/liblzma/delta")
-cCompile(path / "liblzma/src/common")
-cCompile(path / "zlib/*.c", exclude="contrib,examples,test")
-
